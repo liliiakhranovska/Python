ce72db3df375c2f9c04089597c0dab865a41925d
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-05-19 23:42:24 +0300

    iter_while_vacant function was added instead of diagonal and remove_moves function

--- src/domain/bishop.py
+++ src/domain/bishop.py
@@ -2,53 +2,24 @@ import math
 
 WHOLE_BOARD = frozenset([(x, y) for x in range(8) for y in range(8)])
 
-def __iter_while_vacant(point, board):
+
+def __iter_possible_moves(point, board):
     x, y = point
     _, initial_player = board[x][y]
-    while (x+1, y+1) in WHOLE_BOARD:
-        if board[x+1][y+1] == None:
-            yield x+1, y+1
-        if board[x+1][y+1] != None:
-            _, player = board[x+1][y+1]
-            if initial_player != player:
-                yield x+1, y+1
-            break
-        x += 1
-        y += 1
-    x, y = point
-    while (x-1, y+1) in WHOLE_BOARD:
-        if board[x-1][y+1] == None:
-            yield x-1, y+1
-        if board[x-1][y+1] != None:
-            _, player = board[x-1][y+1]
-            if initial_player != player:
-                yield x-1, y+1
-            break
-        x -= 1
-        y += 1
-    x, y = point
-    while (x-1, y-1) in WHOLE_BOARD:
-        if board[x-1][y-1] == None:
-            yield x-1, y-1
-        if board[x-1][y-1] != None:
-            _, player = board[x-1][y-1]
-            if initial_player != player:
-                yield x-1, y-1
-            break
-        x -= 1
-        y -= 1
-    x, y = point
-    while (x+1, y-1) in WHOLE_BOARD:
-        if board[x+1][y-1] == None:
-            yield x+1, y-1
-        if board[x+1][y-1] != None:
-            _, player = board[x+1][y-1]
-            if initial_player != player:
-                yield x+1, y-1
-            break
-        x += 1
-        y -= 1
+    directions = [(step_for_x, step_for_y) for step_for_x in (-1,1) for step_for_y in (-1,1)]
+    for direction in directions:
+        x, y = point
+        while (x+direction[0], y+direction[1]) in WHOLE_BOARD:
+            if board[x+direction[0]][y+direction[1]] == None:
+                yield x+direction[0], y+direction[1]
+            if board[x+direction[0]][y+direction[1]] != None:
+                _, player = board[x+direction[0]][y+direction[1]]
+                if player != initial_player:
+                    yield x+direction[0], y+direction[1]
+                break
+            x += direction[0]
+            y += direction[1]
 
 
 def try_to_move_bishop(point, move_point, board):
-    return move_point in __iter_while_vacant(point, board)
+    return move_point in __iter_possible_moves(point, board)
