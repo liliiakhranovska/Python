7afb744392ac1e925e03635950a0f6251c0912e2
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-05-20 23:06:16 +0300

    amended south_east

--- src/domain/bishop.py
+++ src/domain/bishop.py
@@ -5,7 +5,7 @@ def try_to_move(current_point, target_point, board):
         return [(x, y) for x in range(current_x + 1, 8) for y in range(current_y + 1, 8) if x-current_x == y-current_y]
 
     def south_east():
-        return [(x, y) for x in range(current_x + 1, 8) for y in range(current_y, -1, -1) if x-current_x == current_y-y]
+        return [(x, y) for x in range(current_x + 1, 8) for y in range(current_y-1, -1, -1) if x-current_x == current_y-y]
 
     def south_west():
         return [(x, y) for x in range(current_x-1, -1, -1) for y in range(current_y-1, -1, -1) if x-current_x == y-current_y]
