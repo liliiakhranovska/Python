f2094103717acf4755d2dfcd73c5d1f5f8fc80ef
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-05-20 23:02:17 +0300

    def north_east, south_east,south_west, north_west were changed cause they generated excessive values and for def iter_reachable_points direction matters; to iter_reachable_points player condition was added

--- src/domain/bishop.py
+++ src/domain/bishop.py
@@ -5,13 +5,13 @@ def try_to_move(current_point, target_point, board):
         return [(x, y) for x in range(current_x + 1, 8) for y in range(current_y + 1, 8) if x-current_x == y-current_y]
 
     def south_east():
-        return [(x, y) for x in range(current_x + 1, 8) for y in range(0, current_y) if x-current_x == current_y-y]
+        return [(x, y) for x in range(current_x + 1, 8) for y in range(current_y, -1, -1) if x-current_x == current_y-y]
 
     def south_west():
-        return [(x, y) for x in range(0, current_x) for y in range(0, current_y) if x-current_x == y-current_y]
+        return [(x, y) for x in range(current_x-1, -1, -1) for y in range(current_y-1, -1, -1) if x-current_x == y-current_y]
 
     def north_west():
-        return [(x, y) for x in range(0, current_x) for y in range(current_y + 1, 8)if x-current_x == current_y-y]
+        return [(x, y) for x in range(current_x-1, -1, -1) for y in range(current_y + 1, 8)if x-current_x == current_y-y]
 
     def iter_reachable_points():
         _, initial_player = board[current_x][current_y]
@@ -22,21 +22,21 @@ def try_to_move(current_point, target_point, board):
                     yield x, y
                 break
             yield x, y
-            for (x, y) in south_east():
+        for (x, y) in south_east():
             if board[x][y] is not None:
                 _, player = board[x][y]
                 if player != initial_player:
                     yield x, y
                 break
             yield x, y
-            for (x, y) in south_west():
+        for (x, y) in south_west():
             if board[x][y] is not None:
                 _, player = board[x][y]
                 if player != initial_player:
                     yield x, y
                 break
             yield x, y
-            for (x, y) in north_west():
+        for (x, y) in north_west():
             if board[x][y] is not None:
                 _, player = board[x][y]
                 if player != initial_player:
