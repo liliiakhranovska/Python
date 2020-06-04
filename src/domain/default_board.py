caeea03f309340bde2f64afee9cab27c2214eeb0
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-06-04 23:44:07 +0300

    added default_board

---
+++ src/domain/default_board.py
@@ -0,0 +1,15 @@
+import players
+import chessset
+
+
+def default_board():
+    return [[(chessset.ROOK, players.WHITE_PLAYER), (chessset.KNIGHT, players.WHITE_PLAYER), (chessset.BISHOP, players.WHITE_PLAYER), (chessset.QUEEN, players.WHITE_PLAYER), (chessset.KING, players.WHITE_PLAYER), (chessset.BISHOP, players.WHITE_PLAYER), (chessset.KNIGHT, players.WHITE_PLAYER), (chessset.ROOK, players.WHITE_PLAYER)],
+            [(chessset.PAWN, players.WHITE_PLAYER), (chessset.PAWN, players.WHITE_PLAYER), (chessset.PAWN, players.WHITE_PLAYER), (chessset.PAWN, players.WHITE_PLAYER),
+             (chessset.PAWN, players.WHITE_PLAYER), (chessset.PAWN, players.WHITE_PLAYER), (chessset.PAWN, players.WHITE_PLAYER), (chessset.PAWN, players.WHITE_PLAYER)],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [(chessset.PAWN, players.BLACK_PLAYER), (chessset.PAWN, players.BLACK_PLAYER), (chessset.PAWN, players.BLACK_PLAYER), (chessset.PAWN, players.BLACK_PLAYER),
+             (chessset.PAWN, players.BLACK_PLAYER), (chessset.PAWN, players.BLACK_PLAYER), (chessset.PAWN, players.BLACK_PLAYER), (chessset.PAWN, players.BLACK_PLAYER)],
+            [(chessset.ROOK, players.BLACK_PLAYER), (chessset.KNIGHT, players.BLACK_PLAYER), (chessset.BISHOP, players.BLACK_PLAYER), (chessset.QUEEN, players.BLACK_PLAYER), (chessset.KING, players.BLACK_PLAYER), (chessset.BISHOP, players.BLACK_PLAYER), (chessset.KNIGHT, players.BLACK_PLAYER), (chessset.ROOK, players.BLACK_PLAYER)]]
--- src/domain/king.py
+++ src/domain/king.py
@@ -2,12 +2,8 @@ def try_to_move(current_point, target_point, board):
     current_x, current_y = current_point
     _, initial_player = board[current_x][current_y]
 
-    def move():
-        return [(x, y) for x in range(current_x-1, current_x + 2) for y in range(current_y-1, current_y + 2) if (x, y) != (current_x, current_y) and x > -1 and y > -1 and x < 8 and y < 8]
-
     def iter_reachable_points():
-        for movement in move():
-            x, y = movement
+        for (x,y) in [(x, y) for x in range(current_x-1, current_x + 2) for y in range(current_y-1, current_y + 2) if (x, y) != (current_x, current_y) and x > -1 and y > -1 and x < 8 and y < 8]:
             if board[x][y] is not None:
                 _, player = board[x][y]
                 if player != initial_player:
