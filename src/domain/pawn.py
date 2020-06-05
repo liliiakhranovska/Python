ece8ac487c26dcd5df908203ae9d9b1bf830100c
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-06-05 14:53:52 +0300

    optimized pawn.py, remove test4.py from dev directory

--- src/domain/pawn.py
+++ src/domain/pawn.py
@@ -1,28 +1,31 @@
+import players
+
+
 def try_to_move(current_point, target_point, board):
     current_x, current_y = current_point
-    _, initial_player = board[current_x][current_y] 
+    _, initial_player = board[current_x][current_y]
 
-    def move_white():
+    def pos_moves_white():
         if current_x + 1 < 8:
             yield current_x + 1, current_y
         if current_x == 1:
             if board[2][current_y] is None:
                 yield current_x + 2, current_y
 
-    def take_white():
+    def pos_takes_white():
         if current_x + 1 < 8 and current_y + 1 < 8:
             yield current_x + 1, current_y + 1
         if current_x + 1 < 8 and current_y - 1 > -1:
             yield current_x + 1, current_y - 1
 
-    def move_black():
+    def pos_moves_black():
         if current_x - 1 > -1:
             yield current_x - 1, current_y
         if current_x == 6:
             if board[5][current_y] is None:
                 yield current_x - 2, current_y
 
-    def take_black():
+    def pos_takes_black():
         if current_x - 1 > -1 and current_y + 1 < 8:
             yield current_x - 1, current_y + 1
         if current_x - 1 > -1 and current_y - 1 > -1:
@@ -30,22 +33,21 @@ def try_to_move(current_point, target_point, board):
 
     def iter_reachable_points():
         if initial_player == players.WHITE_PLAYER:
-            for move in move_white():
-                x, y = move
+            for x, y in pos_moves_white():
                 if board[x][y] is None:
                     yield x, y
-                for move in take_white():
+                for move in pos_takes_white():
                     x, y = move
                     if board[x][y] is not None:
                         _, player = board[x][y]
                         if player == players.BLACK_PLAYER:
                             yield x, y
-        if initial_player == players.BLACK_PLAYER:
-            for move in move_black():
+        elif initial_player == players.BLACK_PLAYER:
+            for move in pos_moves_black():
                 x, y = move
                 if board[x][y] is None:
                     yield x, y
-            for move in take_black():
+            for move in pos_takes_black():
                 x, y = move
                 if board[x][y] is not None:
                     _, player = board[x][y]
