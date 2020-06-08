1cf1f65bf083265b7de811a812c2f904124d665c
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-06-08 18:07:01 +0300

    optimized queen

--- src/domain/queen.py
+++ src/domain/queen.py
@@ -3,5 +3,4 @@ import rook
 
 
 def try_to_move(current_point, target_point, board):
-    if bishop.try_to_move (current_point, target_point, board) is True or bishop.try_to_move (current_point, target_point, board) is True:
-        return True
+    return bishop.try_to_move (current_point, target_point, board) or bishop.try_to_move (current_point, target_point, board)
