52af8079e090ce32b079e85f70c69beb0620074b
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-06-08 17:59:55 +0300

    queen

---
+++ src/domain/queen.py
@@ -0,0 +1,7 @@
+import bishop
+import rook
+
+
+def try_to_move(current_point, target_point, board):
+    if bishop.try_to_move (current_point, target_point, board) is True or bishop.try_to_move (current_point, target_point, board) is True:
+        return True
