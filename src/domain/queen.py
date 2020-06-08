131c20c0cbce71f55f913b48a6745f0d73414c86
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-06-08 18:11:10 +0300

    typo fixed

--- src/domain/queen.py
+++ src/domain/queen.py
@@ -3,4 +3,4 @@ import rook
 
 
 def try_to_move(current_point, target_point, board):
-    return bishop.try_to_move (current_point, target_point, board) or rook.try_to_move (current_point, target_point, board)
+    return bishop.try_to_move(current_point, target_point, board) or rook.try_to_move(current_point, target_point, board)
