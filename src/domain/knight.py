9a985abdc6327ac168c833bbf3c55842c7bfdea9
KolyaTimoshenko <58301205+KolyaTimoshenko@users.noreply.github.com>
2020-06-09 15:39:00 +0300

    Add changes to knight.py and test_knight.py files

--- src/domain/knight.py
+++ src/domain/knight.py
@@ -1,46 +1,20 @@
-
 def try_to_move(current_point, target_point, board):
     current_x, current_y = current_point
     _, initial_player = board[current_x][current_y] 
     
-    def nnw():
-        return current_x - 1, current_y + 2
-
-    def wnw():
-        return current_x - 2, current_y + 1
-
-    def wsw():
-        return current_x - 2, current_y - 1
-
-    def ssw():
-        return current_x - 1, current_y - 2
-
-    def sse():
-        return current_x + 1, current_y - 2
-
-    def ese():
-        return current_x + 2, current_y - 1
-
-    def ene():
-        return current_x + 2, current_y + 1
-
-    def nne():
-        return current_x + 1, current_y + 2
-
+    def pos_moves():
+        return [(x, y) for x in range(8) for y in range(8) if (abs(x - current_x) == 1 and abs(y - current_y) == 2) or (abs(x - current_x) == 2 and abs(y - current_y) == 1)]
     
     def iter_reachable_points():
-        for direction in (nnw, wnw, wsw, ssw, sse, ese, ene, nne):
-            current_x, current_y = direction()
-            if board[current_x][current_y] is not None:
-                    _, player = board[current_x][current_y]
-                    if player != initial_player:
-                        yield current_x, current_y
-                    break
-            yield current_x, current_y
+        for (x, y) in pos_moves():
+            if board[x][y] is not None:
+              _, player = board[x][y]
+              if player != initial_player:
+                    yield x, y
+              break
+            yield x, y
   
-    return target_point in set(iter_reachable_points())   
-        
-
+    return target_point in set(iter_reachable_points())
 
 
 
--- src/domain/test_knight.py
+++ src/domain/test_knight.py
@@ -7,18 +7,7 @@ import unittest
 
 
 class TestRook(unittest.TestCase):
-    
-    def test_knight_name_figure(self):
-        self.assertEqual(chessset.KNIGHT, 'KNIGHT')
-        self.assertNotEqual(chessset.KNIGHT, 'knightt')
-
-    def test_if_knight_has_correct_initially_coordinates_at_the_beginning_of_the_game(self):
-        self.assertTrue(test_utils.default_board[0][1], ('KNIGHT', 'WHITE_PLAYER'))
-        self.assertTrue(test_utils.default_board[0][6], ('KNIGHT', 'WHITE_PLAYER'))
-        self.assertTrue(test_utils.default_board[7][1], ('KNIGHT', 'BLACK_PLAYER'))
-        self.assertTrue(test_utils.default_board[7][6], ('KNIGHT', 'BLACK_PLAYER'))     
-
-
+ 
     def test_that_knight_can_move_north_north_west(self):
         board = test_utils.empty_board
         board[2][2] = (chessset.KNIGHT, players.BLACK_PLAYER)
@@ -55,9 +44,9 @@ class TestRook(unittest.TestCase):
 
     def test_that_knight_can_move_east_south_east(self):
         board = test_utils.empty_board
-        board[2][2] = (chessset.KNIGHT, players.BLACK_PLAYER)
-        board[3][2] = (chessset.BISHOP, players.WHITE_PLAYER)
-        self.assertTrue(knight.try_to_move((2, 2), (4, 1), board))
+        board[5][5] = (chessset.KNIGHT, players.BLACK_PLAYER)
+        board[6][4] = (chessset.BISHOP, players.WHITE_PLAYER)
+        self.assertTrue(knight.try_to_move((5, 5), (7, 4), board))
 
 
     def test_that_knight_can_move_east_north_east(self):
