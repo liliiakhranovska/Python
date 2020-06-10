995b30cd98ba618dfe810a029243b5e2304ac5d4
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-06-10 19:54:26 +0300

    bishop refactoring. added directions.py

--- src/domain/queen.py
+++
@@ -1,6 +0,0 @@
-import bishop
-import rook
-
-
-def try_to_move(current_point, target_point, board):
-    return bishop.try_to_move(current_point, target_point, board) or rook.try_to_move(current_point, target_point, board)
