7cf1f5235bfae0c77188bcf7c82dee810bd50672
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-06-16 18:48:31 +0300

    king refactored using directions.py

--- src/domain/king.py
+++ src/domain/king.py
@@ -1,14 +1,19 @@
+import directions
+
+
 def try_to_move(current_point, target_point, board):
     current_x, current_y = current_point
-    _, initial_player = board[current_x][current_y]
 
     def iter_reachable_points():
-        for (x, y) in [(x, y) for x in range(current_x-1, current_x + 2) for y in range(current_y-1, current_y + 2) if (x, y) != (current_x, current_y) and x > -1 and y > -1 and x < 8 and y < 8]:
-            if board[x][y] is not None:
-                _, player = board[x][y]
-                if player != initial_player:
-                    yield x, y
-            else:
+        _, initial_player = board[current_x][current_y]
+        for direction in (directions.north_east(current_x, current_y), directions.south_east(current_x, current_y), directions.south_west(current_x, current_y), directions.north_west(current_x, current_y), directions.north(current_x, current_y), directions.south(current_x, current_y), directions.west(current_x, current_y), directions.east(current_x, current_y)):
+            for (x, y) in direction:
+                if board[x][y] is not None:
+                    _, player = board[x][y]
+                    if player != initial_player:
+                        yield x, y
+                        break
+                    break
                 yield x, y
-
+                break
     return target_point in set(iter_reachable_points())
