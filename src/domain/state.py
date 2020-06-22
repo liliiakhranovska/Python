afb107ee3bd5e8851b9f8dc1e7c2d6fa3d6d911c
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-06-22 22:05:27 +0300

    game,state

---
+++ src/domain/state.py
@@ -0,0 +1,6 @@
+white_king_has_moved = False
+black_king_has_moved = False
+white_sw_rook_has_moved = False
+white_nw_rook_has_moved = False
+black_se_rook_has_moved = False
+black_sw_rook_has_moved = False
