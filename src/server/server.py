4941c39b6830682c19f952ed09cfcab3025c5c0b
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-09-06 13:10:08 +0300

    added 404 code responce if wrong game id, changed json repsponce to dictionary

--- src/server/server.py
+++ src/server/server.py
@@ -4,12 +4,16 @@ import os
 sys.path.append(os.path.join(sys.path[0], '../domain'))
 from flask import jsonify
 from flask import Flask
+from flask import abort
 app = Flask(__name__)
 
 @app.route("/games/<int:id>", methods=['GET'])
 def show_user_profile(id):
     if id ==32:
-        return jsonify(fake_data.board, fake_data.moves_counter, fake_data.next_player)
+        return jsonify(board = fake_data.board, moves_counter = fake_data.moves_counter, next_player = fake_data.next_player)
+    else:
+        abort(404)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4567)
