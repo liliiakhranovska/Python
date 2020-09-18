4e33adc8708b8145607ce58ebffea928dd719a0e
Liliia Khranovska <lilia.khranovska@gmail.com>
2020-09-18 19:32:20 +0300

    def convert_GameState_instance_to_json was added, responces for all end-points were amended to be in json

--- src/server/gateway.py
+++ src/server/gateway.py
@@ -1,27 +1,26 @@
-import sys
 import os
+import sys
 sys.path.append(os.path.join(sys.path[0], '../domain'))
 import default_board
 import players
 
 
-class BoardGame:
+class GameState:
     def __init__(self, 
-                board = list(map(lambda row: list(map(lambda cell: {'piece': cell[0], ' colour': cell[1]} if cell != None else cell, row)), default_board.default_board())), 
-                moves_counter = 0, 
-                next_player = players.WHITE_PLAYER):
+                board,
+                moves_counter=0, 
+                next_player=players.WHITE_PLAYER):
+        if board == None:
+            board = default_board.default_board()
         self.board = board
         self.moves_counter = moves_counter
         self.next_player = next_player
 
-__db = {32: BoardGame()}
+__db = {32: GameState(None)}
 
 def get_state_by_id(id):
-    if id in __db:
-        return __db[id].board, __db[id].moves_counter, __db[id].next_player
-    else:
-        return None
+    return __db.get(id)
 
-def new_game():
-    __db[max(__db.keys())+1] = BoardGame()
+def post_new_game():
+    __db[max(__db.keys())+1] = GameState(None)
     return max(__db.keys())
--- src/server/server.py
+++ src/server/server.py
@@ -1,25 +1,32 @@
+import os
+import sys
+import json
 from flask import Flask, abort, jsonify, Response
 import gateway
 import default_board
 import players
-import sys
-import os
-import json
+
 sys.path.append(os.path.join(sys.path[0], '../domain'))
 app = Flask(__name__)
 
+def convert_GameState_instance_to_json(instance):
+    board = instance.board
+    moves_counter = instance.moves_counter
+    next_player = instance.next_player
+    json_board = list(map(lambda row: list(map(lambda cell: {'piece': cell[0], 'colour': cell[1]} if cell != None else cell, row)), board))
+    return {'board': json_board, 'moves_counter': moves_counter, 'next_player': next_player}
 
 @app.route("/games/<int:id>", methods=['GET'])
 def show_game_status(id):
     if gateway.get_state_by_id(id) is not None:
-        return jsonify(gateway.get_state_by_id(id))
+        return convert_GameState_instance_to_json(gateway.get_state_by_id(id))
     else:
         abort(404)
 
 @app.route("/games", methods=['POST'])
-def create_game():
-    if gateway.new_game():
-        return Response('Game %s is created' % (gateway.new_game()), status=201, mimetype='application/json')
+def create_new_game():
+    id = gateway.post_new_game()
+    return Response("{'Game_id': %s}" % (id), status=201, mimetype='application/json')
 
 
 if __name__ == "__main__":
